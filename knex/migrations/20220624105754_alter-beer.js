8a90d17147bb9fbd56a007b2298dc6c71314f921
jordan.servant@yahoo.fr <jordan.servant@yahoo.fr>
2022-06-24 13:39:07 +0200

    knex

--- knex/migrations/20220624105754_alter-beer.js
+++ knex/migrations/20220624105754_alter-beer.js
@@ -6,6 +6,13 @@ exports.up = knex => knex.schema.alterTable(TABLE_NAME, (table) => {
       .unsigned()
       .references("id")
       .inTable("COMPANYS");
+    }).then(function () {
+        return knex(TABLE_NAME).insert([
+            {nom: "Koko", volume: "0.33cl",companyId: 1},
+            {nom: "Delirium", volume: "0.33cl",companyId: 2},
+            {nom: "Lefe", volume: "0.33cl",companyId: 3},
+            {nom: "Duff", volume: "0.50cl",companyId: 4}
+    ]);
 });
 
 exports.down = function(knex) {};
